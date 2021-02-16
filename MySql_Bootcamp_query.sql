3f403e354dfa75dd6ddfeb43a7036717adf7131d
Florin Gabriel Abagiu <35663669+gaby911@users.noreply.github.com>
2021-02-16 17:40:09 +0200

    add instagram db example

--- MySql_Bootcamp_query.sql
+++ MySql_Bootcamp_query.sql
@@ -842,4 +842,65 @@ join reviews
 join series 
 	on series.id = reviews.series_id
 ORDER BY title;
-    
+   
+create DATABASE ig_clone;
+
+use ig_clone;
+
+create table users(
+id integer AUTO_INCREMENT PRIMARY key,
+username varchar(255) UNIQUE not null,
+created_at timestamp DEFAULT now()
+);
+
+create table photos(
+id int AUTO_INCREMENT PRIMARY key,
+image_url VARchar(255) not null,
+user_id int not null,
+created_at timestamp default now(),
+foreign key (user_id) references users(id)
+);
+
+create table comments(
+id int AUTO_INCREMENT primary key,
+comment_text varchar(255) not null,
+user_id int not null,
+photo_id int not null,
+created_at timestamp default now(),
+foreign key(user_id) references users(id),
+foreign key(photo_id) references photos(id)
+);
+-- primary key(user_id, photo_id) will help you have only one like-- 
+create table likes(
+user_id int not null,
+photo_id int not null,
+created_at timestamp default now(),
+foreign key(user_id) references users(id),
+foreign key (photo_id) references photos(id),
+primary key(user_id, photo_id) 
+);
+
+create table follows(
+follower_id int not null,
+followee_id int not null,
+created_at timestamp default now(),
+foreign key(follower_id) references users(id),
+foreign key(followee_id) references users(id),
+primary key(follower_id, followee_id)
+);
+
+desc follows;
+
+create table tags(
+id int AUTO_INCREMENT primary key,
+tag_name varchar(255) UNIQUE,
+created_at timestamp default now()
+);
+
+create table photo_tags(
+photo_id int not null,
+tag_id int not null,
+FOREIGN KEY (photo_id) references photos(id),
+FOREIGN KEY (tag_id) references tags(id),
+primary key(photo_id, tag_id)
+);
